46604a2e6d6478fca23d593393b1467dc97c11c8
sudatra <mail2sudatra@gmail.com>
2025-05-07 20:37:35 +0530

    added resolve function to redirect user to actual url from short url

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -6,3 +6,23 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url");
+
+	r := database.CreateClient(0);
+	defer r.Close();
+
+	value, err := r.Get(database.Ctx, url).Result();
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Shortened URL not found in database"});
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot connect to DB"});
+	}
+
+	rInr := database.CreateClient(1);
+	defer rInr.Close();
+
+	_ = rInr.Incr(database.Ctx, "counter");
+
+	return c.Redirect(value, 301);
+}
